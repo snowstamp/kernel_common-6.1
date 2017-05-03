5ccf181e1c6adb2aa3916102bdf51a31a329c206
Daniel Micay <danielmicay@gmail.com>
2017-05-03 11:38:54 -0400

    bug on cache_from_obj !PageSlab check
    Change-Id: I2db136b506da7d982db69dc614eef55897f6e87b
    Signed-off-by: Daniel Micay <danielmicay@gmail.com>
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>
    Signed-off-by: randomhydrosol <randomhydrosol@glassrom.org>

--- mm/slab.h
+++ mm/slab.h
@@ -652,10 +652,14 @@ static inline struct kmem_cache *virt_to_cache(const void *obj)
 {
 	struct slab *slab;
 
+#ifdef CONFIG_BUG_ON_DATA_CORRUPTION
+	BUG_ON(!slab_page(slab))
+#else
 	slab = virt_to_slab(obj);
 	if (WARN_ONCE(!slab, "%s: Object is not a Slab page!\n",
 					__func__))
 		return NULL;
+#endif
 	return slab->slab_cache;
 }
 
